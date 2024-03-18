8deedd72b2479386b263f51a581b417d2054e4aa
Leah Lariscy <lmlariscy@uga.edu>
2024-03-17 22:27:51 -0400

    Update HW3.sh

--- HW2.sh
+++ HW2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=homework_1  		                    # Job name
+#SBATCH --job-name=homework_2  		                    # Job name
 #SBATCH --partition=batch		                        # Partition (queue) name
 #SBATCH --ntasks=1			                            # Single task job
 #SBATCH --cpus-per-task=4		                        # Number of cores per task - match this to the num_threads used by BLAST
