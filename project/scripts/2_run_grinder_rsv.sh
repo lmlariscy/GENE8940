0c972cb516a8689ebb1de0183615671f7a0ceaf9
Leah Lariscy <lmlariscy@uga.edu>
2024-04-24 20:54:18 -0400

    Update 2_run_grinder_rsv.sh

--- project/scripts/2_run_grinder_rsv.sh
+++ project/scripts/2_run_grinder_rsv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=grinder_sim_reads_rsv	            # Job name
+#SBATCH --job-name=pull_ww_sra		                    # Job name
 #SBATCH --partition=batch		                        # Partition (queue) name
-#SBATCH --ntasks=12			                            # Single task job
+#SBATCH --ntasks=1			                            # Single task job
 #SBATCH --cpus-per-task=4		                        # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                            # Total memory for job
-#SBATCH --time=90:00:00  		                        # Time limit hrs:min:sec
+#SBATCH --time=2:00:00  		                        # Time limit hrs:min:sec
 #SBATCH --output=/work/gene8940/lml38336/log.%j			# Location of standard output and error log files (replace lml38336 with your myid)
 #SBATCH --mail-user=lml38336@uga.edu                    # Where to send mail (replace lml38336 with your myid)
 #SBATCH --mail-type=BEGIN,END,FAIL                      # Mail events (BEGIN, END, FAIL, ALL)
