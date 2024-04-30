c77d3551e2f7de94e929bbd58c2bac4aa3c72f96
Leah Lariscy <lmlariscy@uga.edu>
2024-04-30 11:37:06 -0400

    update kallisto scripts

--- project/scripts/5_run_kallisto/2_kallisto_idx.sh
+++ project/scripts/5_run_kallisto/2_kallisto_idx.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=Kallisto_indexing
-#SBATCH --partition=partition
-#SBATCH --ntasks=1
-#SBATCH --mem=#
-#SBATCH --export=
-#SBATCH --time=##:##:##
-#SBATCH --output=%x_%j.out
-#SBATCH --error=%x_%j.err
-#SBATCH --mail-user=$$$$$$@email.edu
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=batch		                        
+#SBATCH --ntasks=1			                            
+#SBATCH --cpus-per-task=10		                        
+#SBATCH --mem=40gb			                            
+#SBATCH --time=2:00:00  		                       
+#SBATCH --output=/work/gene8940/lml38336/log.%j			
+#SBATCH --mail-user=lml38336@uga.edu                    
+#SBATCH --mail-type=BEGIN,END,FAIL 
 
+#load module
 module load kallisto/0.48.0-gompi-2022a
 
 #Set directory
-cd /file/path/to/reference_sequences
+cd /work/gene8940/lml38336/project/references/all
+ref_seq='covid_flu_rsv_refseq.fasta'
 
-kallisto index -i sequences.kallisto_idx ref_seqs.fasta
+
+kallisto index -i sequences.kallisto_idx $ref_seq
